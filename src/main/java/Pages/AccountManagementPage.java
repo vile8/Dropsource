38976d1a3bda2316180facf1d9df399378d7cd22
Jonathan Doll <jdoll@queuesoftware.com>
2016-09-07 13:31:22 -0400

    Adding in account management functions Removed some android stuff

--- src/main/java/Pages/AccountManagementPage.java
+++ src/main/java/Pages/AccountManagementPage.java
@@ -59,6 +59,18 @@ public class AccountManagementPage extends Page{
         return driver.findElement(By.xpath("//div[@data-test='profile-image']"));
     }
     
+    private WebElement currentPasswordField(){
+        return driver.findElement(By.xpath("//input[@data-test='previous-password-input']"));
+    }
+    
+    private WebElement newPasswordField(){
+        return driver.findElement(By.xpath("//input[@data-test='new-password-input']"));
+    }
+    
+    private WebElement confirmPasswordField(){
+        return driver.findElement(By.xpath("//input[@data-test='verify-password-input']"));
+    }
+    
     public void changeName(String firstName, String lastName){
         action.moveToElement(name()).perform();
         edit().click();
@@ -70,8 +82,15 @@ public class AccountManagementPage extends Page{
         while(System.currentTimeMillis() - timer < 10000 && btnSaveExists());
     }
     
-    public void changePassword(){
+    public void changePassword(String currentPassword, String newPassword, String confirmNewPassword){
         btnChangePassword().click();
+        wait.animation();
+        currentPasswordField().sendKeys(currentPassword);
+        newPasswordField().sendKeys(newPassword);
+        confirmPasswordField().sendKeys(confirmNewPassword);
+        btnSave().click();
+        long timer = System.currentTimeMillis();
+        while(System.currentTimeMillis() - timer < 10000 && btnSaveExists());
     }
     
     public void addProfilePhoto(){
